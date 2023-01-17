eac867b4f87f21c748a403e02f7d3d3709e0ff3f
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-17 03:54:38 +0100

    Create components fot different sections of the webapp

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Header from "./Header";
+import TodoList from "./TodoList";
+import InputTodo from "./InputTodo";
+
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      },
+    ],
+  };
+
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }))
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <InputTodo />
+        <TodoList todos={this.state.todos} handleChangeProps={this.handleChange}
+        deleteTodoProps={this.delTodo} />
+      </div>
+    )
+  }
+}
+
+export default TodoContainer
